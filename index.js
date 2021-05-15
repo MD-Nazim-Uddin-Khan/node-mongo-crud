76c7b5c75962cd05845c18b1238c816867e6a87a
MD-Nazim-Uddin-Khan <mdnazimuddin5500@gmail.com>
2021-05-15 12:59:53 +0600

    mongo hulp

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ client.connect(err => {
         productCollection.insertOne(product)
         .then(result =>{
             console.log('data added successfully');
-            res.send('success')
+            res.redirect('/');
         })
     })
 
@@ -61,10 +61,20 @@ client.connect(err => {
         })
     })
 
+    app.patch('/update/:id', (req, res)=>{
+        productCollection.updateOne({_id: ObjectId(req.params.id)},
+        {
+            $set: {price: req.body.price, quantity: req.body.quantity}
+        })
+        .then(result => {
+            res.send(result.modifiedCount > 0)
+        })
+    })
+
     app.delete('/delete/:id', (req, res)=>{
         productCollection.deleteOne({_id: ObjectId(req.params.id)})
         .then(result=>{
-            console.log(result);
+            res.send(result.deletedCount > 0)
         })
     })
 });
